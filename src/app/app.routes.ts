ee9aa53aac12251894699afd04c5ca955b51f320
Adenilson Soares <adenilson_denis8@hotmail.com>
2024-03-13 23:54:52 -0300

    Estrutura Banco de Dados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PacienteFormComponent } from './components/paciente-form/paciente-form.component';
+import { NovoPacienteComponent } from './pages/novo-paciente/novo-paciente.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'novo/paciente', component: PacienteFormComponent}
+    {path: 'novo/paciente', component: NovoPacienteComponent}
 ];
 
